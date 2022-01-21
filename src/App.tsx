d09a472bc8a21712990cd45f73fa4b93a24eaa13
Ana Ivanov <ana_ivanovana@yahoo.com>
2022-01-21 13:47:22 +0200

    App: test if .env exists

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,21 @@ import './App.css';
 import Exchange from './componnets/Exchange';
 
 function App() {
-  return (
-    <div className="App">
-        <Exchange />
-    </div>
-  );
+
+  if (!process.env.REACT_APP_API_KEY) {
+    return (
+      <>
+        <p>Please create an api key from <a href="https://fcsapi.com/">this url</a>.</p>
+        <p>Create a .env file and add the process.env.REACT_APP_API_KEY=YOUR_API_KEY_FROM_FCSAPI</p>
+      </>
+    );
+  } else {
+    return (
+      <div className="App">
+          <Exchange />
+      </div>
+    );
+  }
 }
 
 export default App;
